a9b54afd3eecc00be8f0655afebc6731fd8da7a4
Noemi <medve.noemi.nora@student.ms.sapientia.ro>
2022-04-25 19:09:00 +0300

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/FlightsProject.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FlightsProject.dir/main.c.obj"
+  "FlightsProject.exe"
+  "FlightsProject.exe.manifest"
+  "FlightsProject.pdb"
+  "libFlightsProject.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/FlightsProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
